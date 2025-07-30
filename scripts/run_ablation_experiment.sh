b22e93a55488c36c49e79cebe1c26ac33c03b147
tgmorton <thomasmorton88@gmail.com>
2025-07-30 15:56:14 -0700

    change select gpu

--- scripts/run_ablation_experiment.sh
+++ scripts/run_ablation_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=subject-drop-ablative    # Descriptive job name
-#SBATCH --partition=general_gpu_a5000       # GPU partition
+#SBATCH --partition=general_gpu_p6000       # GPU partition
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
